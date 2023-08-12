6e652709302bd2397bbea4cf0a8e91732ae3dc30
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2023-08-12 15:14:27 +0300

    new changes in dev branch

--- scripts/index.js
+++ scripts/index.js
@@ -31,3 +31,37 @@ function handleFormSubmit(evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+const cardsTemplate = document.getElementById('#cards');
+const cardsContent = cardsTemplate.content.cloneNode(true);
+cardsContent.querySelector('photo-gallery__image').src='./images/photo-gallery/alone.jpg';
+cardsContent.querySelector('photo-gallery__description').textContent = 'Дюк Корморант';
+
+parentNode.append(cardsContent); 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
