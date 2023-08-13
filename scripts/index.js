27dbd11637fc35b25424f8972856196665222aa5
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2023-08-13 17:40:37 +0300

    add new functional (likes and delete cards)

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,52 @@
+//Новая реализация попапа через template
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const popup = document.getElementById('popup').content;
+
+// const popupEditProfile = popup.cloneNode(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Открытие и закрытие попапа
+//
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit');
 const closeButton = popup.querySelector('.popup__close');
@@ -32,7 +81,8 @@ function handleFormSubmit(evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-
+//Рендер всех карточек
+//
 const initialCards = [
   {
     name: 'Цветущий маршрут',
@@ -77,3 +127,47 @@ function renderCard(item) {
 }
 
 render();
+
+//Кнопка удаления
+//
+const deleteCardButtons = document.querySelectorAll('.photo-gallery__remove-button');
+
+deleteCardButtons.forEach(function(item){
+  item.addEventListener('click', deleteCard);
+});
+
+function deleteCard(evt){
+  const evtTarget = evt.target; 
+  const a = evtTarget.closest('.photo-gallery__item');
+  a.remove();
+}
+
+//Кнопка like
+//
+const likeCardButton = document.querySelectorAll('.photo-gallery__like-button');
+
+likeCardButton.forEach(function(item){
+  item.addEventListener('click', likeCard);
+});
+
+function likeCard(evt) {
+  const evtTarget = evt.target;
+  evtTarget.classList.toggle('photo-gallery__like-button_active');
+}
+
+//Кнопка добавления карточки
+//
+// const addCardButton = document.querySelector('.profile__add-button');
+// addCardButton.addEventListener('click', );
+
+// function addCard(){
+
+// }
+
+//Скопируем попап и изменим его текст
+//
+
+// const copyPopup = popup.content.cloneNode(true);
+// console.log(copyPopup)
+
+// const copyPopup = document.querySelector('.popup');
